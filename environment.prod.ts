4e774f46cfda8bbb2a077030e52e79f423f44d13
Mackenzie <Mackenzie-98>
2024-09-09 01:32:25 -0500

    feat: add enviironment back

---
+++ environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://52.22.69.136:3000', // URL del backend en Amplify
+};
--- src/app/shared/service/cut-section/cut-section.service.ts
+++ src/app/shared/service/cut-section/cut-section.service.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../../enviroment.prod';
+import { environment } from '../../../../../environment.prod';
 
 @Injectable({
     providedIn: 'root'
